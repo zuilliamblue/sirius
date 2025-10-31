aa0d126edf35e19f940f24269bef9e1fde296141
zuilliamblue <wmgcomercial@gmail.com>
2025-10-31 18:00:31 -0300

    auto: sync 2025-10-31 18:00:31

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -72,7 +72,7 @@ export default function Header() {
             <img
               src="/brand/SIRIUS.png"
               alt="Sirius"
-              className="h-12 w-auto md:h-14 lg:h-16 object-contain"
+              className="h-16 w-auto md:h-20 lg:h-24 object-contain"
               loading="eager"
             />
           </div>
